e3b93608ff76a4a338d3bf266b9b4db6e76e7b75
Fabian25 <andres.25.9@hotmail.com>
2019-02-28 00:03:44 -0600

    PUSH

---
+++ Tarea3.0/Tarea3.0/MisClases/Parqueo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Tarea3._0.MisClases
+{
+    public class Parqueo
+    {
+        [DataMember]
+        public string ParqueoProvincia { get; set; }
+        [DataMember]
+        public string ParqueoCanton { get; set; }
+        [DataMember]
+        public string ParqueoDistrito { get; set; }
+    }
+}
