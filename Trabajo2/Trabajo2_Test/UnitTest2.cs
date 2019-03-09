29e06d2d71e05a33069bde7b7dd698b77b94dadd
Fabian25 <andres.25.9@hotmail.com>
2019-03-08 23:47:05 -0600

    Updates

---
+++ Trabajo2/Trabajo2_Test/UnitTest2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Trabajo2_Test
+{
+    
+        [TestClass()]
+        public class EstacionamientosTests
+        {
+            [TestMethod()]
+            public void EstacionamientosTest()
+            {
+                //Arrange
+                var unitTest = new Trabajo2_Test.EstacionamientoServices.Service1Client();
+                var provincia = "Cartago";
+                var cantidadDeRegistrosEsperada = 5;
+                var cantidadDeRegistrosReal = 0;
+                var elResultado = unitTest.ListadoderegistrosenEstacionamientoporprovinciaenqueseencuentraelparqueo(provincia);
+
+                //Action
+                if (elResultado != null)
+                    cantidadDeRegistrosReal = elResultado.Count();
+                Console.WriteLine(string.Format("La cantidad de registros consultados es {0}.", elResultado.Count()));
+
+                //Assert
+                Assert.IsTrue(cantidadDeRegistrosEsperada == cantidadDeRegistrosReal);
+            }
+
+        }
+   
+}
